ad4a4e32ffffa9e9d6ce3b3aa691a71efa2460b2
anshikaydv622 <20aydv07@gmail.com>
2026-06-04 22:52:43 +0530

    Fourth commit

---
+++ Day-4/armstronginrange.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int num,temp,digit,sum;
+
+    for(num=1;num<=1000;num++){
+       temp=num;
+       sum=0;
+
+       while(temp!=0){
+        digit=temp%10;
+        sum=sum+digit*digit*digit;
+        temp=temp/10;
+       }
+       if(sum==num){
+        printf("%d",num);
+       }
+    }
+    return 0;
+}
